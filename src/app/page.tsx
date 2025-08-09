4b982d14abac19b7c640b08bb9d51fbfbc30089e
Ashkan <the.epic.2077@gmail.com>
2025-08-10 01:40:32 +0330

    needs changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  redirect("/fa/menu");
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1 className="text-8xl mt-20">Coming Soon ...</h1>
